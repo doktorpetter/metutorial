f25cc450c5a754bed83602628d1bfb6574220c0c
doktorpetter <p.k.risoe@medisin.uio.no>
2016-05-16 22:07:53 +0200

    Add Task.jsx

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,16 @@
+import React, { Component, Proptype } from 'react';
+
+// Task component - represents a single task item
+export default class Task extends Component {
+	render() {
+		return (
+			<li>{this.props.task.text}</li>
+		);
+	}
+}
+
+Task.propTypes = {
+	// This component gets the task to display through a React prop
+	// We can use propTypes to indicate that it is required
+	task: Proptypes.Object.isRequired,
+};
